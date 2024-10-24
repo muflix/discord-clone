363abe7471f67a247af22790d85e51192fe2b404
Eugene <tls.eugene@gmail.com>
2024-10-24 17:22:34 +0300

    add member manage functionality (Change role, kick from server)

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react'
 
 import { EditSeverModal } from '@/components/modals/edit-server-modal'
 import { InviteModal } from '@/components/modals/invite-modal'
+import { MembersModal } from '@/components/modals/members-modal'
 
 export const ModalProvider = () => {
 	const [isMounted, setIsMounted] = useState(false)
@@ -22,6 +23,7 @@ export const ModalProvider = () => {
 			<CreateServerModal />
 			<InviteModal />
 			<EditSeverModal />
+			<MembersModal />
 		</>
 	)
 }
