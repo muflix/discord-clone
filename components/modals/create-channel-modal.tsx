bc1a93fd16b2e76e6e90d10908054dd9673d8dfa
Eugene <tls.eugene@gmail.com>
2024-11-04 17:16:35 +0200

    add Server Channel list

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import qs from 'query-string'
 import { zodResolver } from '@hookform/resolvers/zod'
 import axios from 'axios'
+import qs from 'query-string'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 
@@ -22,17 +22,18 @@ import {
 	FormLabel,
 	FormMessage,
 } from '@/components/ui/form'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue
-} from '@/components/ui/select'
 import { Input } from '@/components/ui/input'
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select'
 import { useModal } from '@/hooks/use-modal-store'
 import { ChannelType } from '@prisma/client'
 import { useParams, useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
 const formSchema = z.object({
 	name: z
@@ -47,30 +48,40 @@ const formSchema = z.object({
 })
 
 export const CreateChannelModal = () => {
-	const { isOpen, onClose, type } = useModal()
+	const { isOpen, onClose, type, data } = useModal()
 	const router = useRouter()
-  const params = useParams()
+	const params = useParams()
 
 	const isModalOpen = isOpen && type === 'createChannel'
+	const { channelType } = data
 
 	const form = useForm({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
 			name: '',
-      type:ChannelType.TEXT
+			type: channelType || ChannelType.TEXT,
 		},
 	})
+
+	useEffect(() => {
+		if (channelType) {
+			form.setValue('type', channelType)
+		} else {
+			form.setValue('type', ChannelType.TEXT)
+		}
+	}),
+		[channelType, form]
 
 	const isLoading = form.formState.isSubmitting
 
 	const onSubmit = async (values: z.infer<typeof formSchema>) => {
 		try {
-      const url = qs.stringifyUrl({
-        url: '/api/channels',
-        query: {
-          serverId: params?.serverId
-        }
-      })
+			const url = qs.stringifyUrl({
+				url: '/api/channels',
+				query: {
+					serverId: params?.serverId,
+				},
+			})
 			await axios.post(url, values)
 
 			form.reset()
@@ -117,41 +128,41 @@ export const CreateChannelModal = () => {
 									</FormItem>
 								)}
 							/>
-              <FormField 
-              control={form.control}
-              name='type'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Channel Type</FormLabel>
-                  <Select
-                  disabled={isLoading}
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                  >
-                    <FormControl>
-                      <SelectTrigger
-                      className='bg-zinc-300/50 border-0 focus:ring-0 text-black ring-offset-0
+							<FormField
+								control={form.control}
+								name='type'
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Channel Type</FormLabel>
+										<Select
+											disabled={isLoading}
+											onValueChange={field.onChange}
+											defaultValue={field.value}
+										>
+											<FormControl>
+												<SelectTrigger
+													className='bg-zinc-300/50 border-0 focus:ring-0 text-black ring-offset-0
                       focus:ring-offset-0 capitalize outline-none'
-                      >
-                        <SelectValue placeholder='Select a channel type' />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      {Object.values(ChannelType).map((type)=>(
-                        <SelectItem
-                        key={type}
-                        value={type}
-                        className='capitalize'
-                        >
-                          {type.toLowerCase()}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                  <FormMessage />
-                </FormItem>
-              )}
-              ></FormField>
+												>
+													<SelectValue placeholder='Select a channel type' />
+												</SelectTrigger>
+											</FormControl>
+											<SelectContent>
+												{Object.values(ChannelType).map(type => (
+													<SelectItem
+														key={type}
+														value={type}
+														className='capitalize'
+													>
+														{type.toLowerCase()}
+													</SelectItem>
+												))}
+											</SelectContent>
+										</Select>
+										<FormMessage />
+									</FormItem>
+								)}
+							></FormField>
 						</div>
 						<DialogFooter className='bg-gray-100 px-6 py-4'>
 							<Button variant='primary' disabled={isLoading}>
--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -4,6 +4,7 @@ import { Member, MemberRole, Profile, Server } from '@prisma/client'
 import { ShieldAlert, ShieldCheck } from 'lucide-react'
 import { useParams, useRouter } from 'next/navigation'
 
+import { UserAvatar } from '@/components/user-avatar'
 import { cn } from '@/lib/utils'
 
 interface ServerMemberProps {
@@ -37,6 +38,10 @@ export const ServerMember = ({ member, server }: ServerMemberProps) => {
 				params?.memberId === member.id && 'bg-zinc-700/20 dark:bg-zinc-700'
 			)}
 		>
+			<UserAvatar
+				src={member.profile.imageUrl}
+				className='h-8 w-8 md:h-8 md:w-8'
+			/>
 			<p
 				className={cn(
 					'font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition',
@@ -44,7 +49,9 @@ export const ServerMember = ({ member, server }: ServerMemberProps) => {
 						'text-primary dark:text-zinc-200 dark:group-hover:text-white'
 				)}
 			>
-				{member.profile.name}
+				{member.profile.name !== 'null null'
+					? member.profile.name
+					: member.profile.email.split('@')[0]}
 			</p>
 			{icon}
 		</button>
